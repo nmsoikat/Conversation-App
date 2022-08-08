d0a234084361d60145bbd5c89fb4c559ac820823
nmsoikat <nursaykat@gmail.com>
2022-08-08 08:50:42 +0600

    login-register ui

--- discord-frontend/src/authPages/loginPage/LoginPage.js
+++ discord-frontend/src/authPages/loginPage/LoginPage.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import AuthBox from '../../shared/components/AuthBox'
+import { validateLoginForm } from '../../shared/utils/validators';
+import LoginPageFooter from './LoginPageFooter';
+import LoginPageHeader from './LoginPageHeader'
+import LoginPageInputs from './LoginPageInputs';
 
-function LoginPage() {
+function LoginPage({ login }) {
+  const navigate = useNavigate();
+
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  useEffect(() => {
+    setIsFormValid(validateLoginForm({ mail, password }));
+  }, [mail, password, setIsFormValid]);
+
+  const handleLogin = () => {
+    const userDetails = {
+      mail,
+      password,
+    };
+
+    login(userDetails, navigate);
+  };
+
   return (
-    <AuthBox>LoginPage</AuthBox>
+    <AuthBox>
+      <LoginPageHeader />
+      <LoginPageInputs
+        mail={mail}
+        setMail={setMail}
+        password={password}
+        setPassword={setPassword}
+      />
+      <LoginPageFooter isFormValid={isFormValid} handleLogin={handleLogin} />
+    </AuthBox>
   )
 }
 
