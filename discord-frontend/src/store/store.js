9f940922e31ae0ca5840ac0c06945409d5575db1
nmsoikat <nursaykat@gmail.com>
2022-09-04 07:01:13 +0600

    message: state and components

--- discord-frontend/src/store/store.js
+++ discord-frontend/src/store/store.js
@@ -5,11 +5,13 @@ import thunk from 'redux-thunk'
 import authReducer from './reduces/authReduce'
 import alertReducer from './reduces/alertReducer'
 import friendsReducer from './reduces/friendsReducer'
+import chatReducer from './reduces/chatReducer'
 
 const rootReducer = combineReducers({
   auth: authReducer,
   alert: alertReducer,
-  friends: friendsReducer
+  friends: friendsReducer,
+  chat: chatReducer
 })
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
