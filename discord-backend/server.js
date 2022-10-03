dfee166864ddedabf3432511f91d04e8454f6b12
nmsoikat <nursaykat@gmail.com>
2022-10-04 00:29:46 +0600

    cors

--- discord-backend/server.js
+++ discord-backend/server.js
@@ -22,7 +22,8 @@ const PORT = process.env.PORT || process.env.API_PORT
 
 const app = express()
 app.use(express.json())
-app.use(cors({origin: '*'}))
+// app.use(cors())
+app.use(cors({origin: 'https://connect-e5rw.onrender.com'}))
 
 
 //register routes
